cb033dfb587e2916a59986b2587617d49904cd9e
Ho Xuan Anh <hoxuananh9.3@gmail.com>
2022-06-29 10:17:56 +0700

    setup box defaultlayout

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
-
+import styles from './Sidebar.module.scss';
+import className from 'classnames/bind';
+const cx = className.bind(styles);
 const Sidebar = () => {
-  return <h2>Sidebar</h2>;
+  return (
+    <aside className={cx('wrapper')}>
+      <h2>sidebar</h2>
+    </aside>
+  );
 };
 
 export default Sidebar;
--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Header from '~/components/Layout/components/Header';
 import Sidebar from './Sidebar';
-
+import styles from './DefaultLayout.module.scss';
+import className from 'classnames/bind';
+const cx = className.bind(styles);
 const DefaultLayout = ({ children }) => {
   return (
-    <div>
+    <div className={cx('wrapper')}>
       <Header />
-      <div className="container">
+      <div className={cx('container')}>
         <Sidebar />
-        <div className="content">{children}</div>
+        <div className={cx('content')}>{children}</div>
       </div>
     </div>
   );
